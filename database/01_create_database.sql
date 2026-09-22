e43cce8c82ce1cf959f8797e53eea8f1a5db3d1b
AdjeMoise <anibeadje@gmail.com>
2026-09-22 15:55:46 -0500

    Create database for retail customer intelligence

---
+++ database/01_create_database.sql
@@ -0,0 +1,6 @@
+-- ============================================================
+-- Retail Customer Intelligence & Campaign Performance Analytics
+-- Database Initialization
+-- ============================================================
+
+CREATE DATABASE retail_customer_intelligence;
